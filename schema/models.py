78869bd48859eb2f036edea1ed4a0a8b71d35553
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-11-14 15:22:45 +0100

    JSON schema for JSON blobs in SpineDB

---
+++ schema/models.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#   "pydantic>=2",
+# ]
+# ///
+
+"""Write JSON schema for JSON blob in SpineDB
+
+"""
+
+# from dataclasses import dataclass
+from datetime import datetime
+from typing import TypeVar
+
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
+
+T = TypeVar("T", str, float, int, datetime)
+
+
+@dataclass(frozen=True)
+class REIndex[T]:
+    name: str
+    values: list[T]
+    run_end: list[int]
+    type: str = "re_index"
+
+
+@dataclass(frozen=True)
+class DEIndex[T]:
+    name: str
+    values: list[T]
+    indices: list[int]
+    type: str = "de_index"
+
+
+@dataclass(frozen=True)
+class Array[T]:
+    name: str
+    values: list[T]
+    type: str = "array"
+
+
+@dataclass(frozen=True)
+class Table:
+    columns: list[REIndex | DEIndex | Array]
+    type: str = "table"
+
+
+@dataclass(frozen=True)
+class Tables:
+    batches: list[Table]
+    type: str = "tables"
+
+
+class JSONBlob(BaseModel):
+    value: Tables | Table
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    import json
+    from pathlib import Path
+
+    parser = ArgumentParser(__doc__)
+    parser.add_argument("json_file", help="Path of JSON schema file to write")
+    opts = parser.parse_args()
+
+    schema = JSONBlob.model_json_schema()
+    Path(opts.json_file).write_text(json.dumps(schema, indent=2))
