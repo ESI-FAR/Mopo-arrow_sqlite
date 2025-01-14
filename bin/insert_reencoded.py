163ef2eb0ede442c0e36cd03e218e1fb656beaeb
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2025-01-14 14:15:15 +0100

    bin/: refactor scripts with options

--- bin/insert_reencoded.py
+++ bin/insert_reencoded.py
@@ -2,25 +2,28 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
-from rich.pretty import pprint
-
 from spinedb_api import DatabaseMapping
 
 if __name__ == "__main__":
+    pvalues = {
+        "flow_node": {"entity_class_name": "flow__node"},
+        "flow_node_no": {"entity_name": "flow__node_wind__80NO"},
+    }
+
     parser = ArgumentParser()
     parser.add_argument("db", help="SQLite Spine database file")
     parser.add_argument("files", nargs="+", help="JSON files to insert")
+    parser.add_argument("-pv", choices=list(pvalues), help="Parameter value selection")
     opts = parser.parse_args()
 
     files = list(map(Path, opts.files))
-    db = DatabaseMapping(f"sqlite:///{opts.db}")
+    db = DatabaseMapping(f"sqlite:///{opts.db}", upgrade=True)
 
-    items = db.get_items("parameter_value", entity_class_name="flow__node")
+    items = db.get_items("parameter_value", **pvalues[opts.pv])
     for item, infile in zip(items, files):
         item.update(value=infile.read_bytes())
-    db.commit_session("Update flow__node to new format")
 
-    # items = db.get_items("parameter_value", entity_name="flow__node_wind__80NO")
-    # for item, infile in zip(items, files):
-    #     item.update(value=infile.read_bytes())
-    # db.commit_session("Update flow__node_wind__80NO to new format")
+    if opts.pv.endswith("_no"):
+        db.commit_session("Update flow__node_wind__80NO to new format")
+    else:
+        db.commit_session("Update flow__node to new format")
--- bin/read_map_timeseries.py
+++ bin/read_map_timeseries.py
@@ -13,12 +13,42 @@ __doc__ = f"""Read map-style timeseries from a Spine db
 
 Run interactively to inspect the timeseries:
 
-$ ipython -i ./{__file__.rsplit("/", 1)[-1]} path/to/db.sqlite
+$ ipython -i ./{__file__.rsplit("/", 1)[-1]} path/to/db.sqlite -pv <parameter value options>
 
 """
 
+db_pv = {
+    "egypt-national.sqlite": ["unit_node_flow"],
+    "BB_data.sqlite": [
+        "grid_node_ts",
+        "grid_node_influx",
+        "flow_node_cf",
+        "flow_node_cf_wind",
+    ],
+}
+pvalues = {
+    "unit_node_flow": {
+        "entity_class_name": "unit__node",
+        "parameter_definition_name": "flow_t",
+    },
+    "grid_node_ts": {
+        "entity_class_name": "grid__node__boundary",
+        "parameter_definition_name": "timeseries",
+    },  # len: [8736, 8736, 8736, 8736, 8736, 8736]
+    "grid_node_influx": {
+        "entity_class_name": "grid__node",
+        "parameter_definition_name": "influx",
+    },  # len: [218400, 218400, 218400, 218400, 218400, 218400, 8736, 8736, 8736, 8736]
+    "flow_node_cf": {
+        "entity_class_name": "flow__node",
+        "parameter_definition_name": "capacityFactor",
+    },
+    "flow_node_cf_wind": {"entity_name": "flow__node_wind__80NO"},
+}
+
 parser = ArgumentParser(__doc__)
 parser.add_argument("db", help="SQLite Spine database file")
+parser.add_argument("-pv", choices=list(pvalues), help="Parameter value selection")
 
 
 def dump_to_file(data: str | bytes, path: str):
@@ -31,45 +61,25 @@ def dump_to_file(data: str | bytes, path: str):
 
 if __name__ == "__main__":
     opts = parser.parse_args()
-    db = DatabaseMapping(f"sqlite:///{opts.db}")
+    db_path = Path(opts.db)
 
-    # # egypt-national.sqlite
-    # ts_rows = db.get_items(
-    #     "parameter_value",
-    #     entity_class_name="unit__node",
-    #     parameter_definition_name="flow_t",
-    # )
-    # ts_list = list(map(lambda i: json_loads_multi_dim_ts(i["value"]), ts_rows))
+    if db_path.name not in db_pv:
+        raise ValueError(f"Unknown db: only supported: {list(db_pv)}")
+    if opts.pv not in db_pv[db_path.name]:
+        raise ValueError(
+            f"Unsupported db parameter value combination: {db_path.name} -> {opts.pv}"
+        )
 
-    # # BB_data.sqlite
-    # ts_rows = db.get_items(
-    #     "parameter_value",
-    #     entity_class_name="grid__node__boundary",
-    #     parameter_definition_name="timeseries",
-    # )
-    # ts_list = list(map(lambda i: json_loads_multi_dim_ts(i["value"]), ts_rows))
-    # # len: [8736, 8736, 8736, 8736, 8736, 8736]
+    db = DatabaseMapping(f"sqlite:///{opts.db}", upgrade=True)
 
-    # influx_rows = db.get_items(
-    #     "parameter_value",
-    #     entity_class_name="grid__node",
-    #     parameter_definition_name="influx",
-    # )
-    # influx_list = list(map(lambda i: json_loads_multi_dim_ts(i["value"]), influx_rows))
-    # # len: [218400, 218400, 218400, 218400, 218400, 218400, 8736, 8736, 8736, 8736]
-
-    # _rows = db.get_items(
-    #     "parameter_value",
-    #     entity_class_name="flow__node",
-    #     parameter_definition_name="capacityFactor",
-    # )
-    _rows = db.get_items("parameter_value", entity_name="flow__node_wind__80NO")
+    _rows = db.get_items("parameter_value", **pvalues[opts.pv])
+    # parsed = list(map(lambda i: json_loads_multi_dim_ts(i["value"]), _rows))
 
     # NOTE: this is a different data structure, so probably need
     # something else to parse (maybe one of the already implemented
     # parsers in dbmap works); dump an example
-    for i, data in enumerate(_rows):
-        # dump_to_file(data["value"], f"capacity-factor-dump-{i}.json")
-        dump_to_file(data["value"], f"wind-node-dump-{i}.json")
-        pprint(json.loads(data["value"]), max_length=4)
-    # parsed = list(map(lambda i: json_loads_multi_dim_ts(i["value"]), _rows))
+    if opts.pv == "flow_node_cf_wind":
+        for i, data in enumerate(_rows):
+            # dump_to_file(data["value"], f"capacity-factor-dump-{i}.json")
+            dump_to_file(data["value"], f"wind-node-dump-{i}.json")
+            pprint(json.loads(data["value"]), max_length=4)
