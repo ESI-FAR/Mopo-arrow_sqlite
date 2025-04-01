b5854f111e4df880a950d900cd511f26381bd23f
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2025-04-01 15:22:20 +0200

    reencode.py: fix to match model rename, and some refactor

--- arrow_expts/schema/reencode.py
+++ arrow_expts/schema/reencode.py
@@ -15,6 +15,7 @@
 from datetime import datetime, timedelta
 import json
 from pathlib import Path
+from typing import cast, overload
 
 import pandas as pd
 import pyarrow as pa
@@ -26,19 +27,22 @@ from ..spine.dbmap import make_records
 from .models import (
     Array,
     ArrayIndex,
-    DEArray,
-    DEIndex,
-    REArray,
-    REIndex,
-    RLIndex,
+    DictEncodedArray,
+    DictEncodedIndex,
+    RunEndArray,
+    RunEndIndex,
+    RunLengthArray,
+    RunLengthIndex,
     Table,
 )
 
 
 def to_df(json_doc: dict):
     data = make_records(json_doc, {}, [], idx_name="metric")
-    tbl = pa.Table.from_pylist(data)
-    df = tbl.to_pandas(types_mapper=pd.ArrowDtype)
+    # NOTE: don't use pyarrow, difficult to support mixed types
+    # tbl = pa.Table.from_pylist(data)
+    # df = tbl.to_pandas(types_mapper=pd.ArrowDtype)
+    df = pd.DataFrame.from_records(data)
     return df
 
 
@@ -46,70 +50,100 @@ class _sentinel:
     pass
 
 
-def rl_encode(arr: ArrayIndex) -> RLIndex:
-    last = _sentinel()
-    values = []
-    run_len = []
+SENTINEL = _sentinel()
+
+
+@overload
+def rl_encode(arr: Array) -> RunLengthArray: ...
+
+
+@overload
+def rl_encode(arr: ArrayIndex) -> RunLengthIndex: ...
+
+
+def rl_encode(arr: ArrayIndex | Array) -> RunLengthIndex | RunLengthArray:
+    last = SENTINEL
+    values, run_len = [], []
     for val in arr.values:
         if val != last:
             values.append(val)
             run_len.append(1)
+            last = val
         else:
             run_len[-1] += 1
-    return RLIndex(name=arr.name, values=values, run_len=run_len)
+    return RunLengthIndex(name=arr.name, values=values, run_len=run_len)
 
 
-def re_encode(arr: ArrayIndex) -> REIndex:
-    last = arr.values[0]  # _sentinel()
-    values = [last]
-    run_end = []
-    for idx, val in enumerate(arr.values[1:], start=1):
-        if val != last:
-            last = val
+@overload
+def re_encode(arr: Array) -> RunEndArray: ...
+
+
+@overload
+def re_encode(arr: ArrayIndex) -> RunEndIndex: ...
+
+
+def re_encode(arr: ArrayIndex | Array) -> RunEndIndex | RunEndArray:
+    last = SENTINEL
+    values, run_end = [], []
+    for idx, val in enumerate(arr.values, start=1):
+        if last != val:
             values.append(val)
             run_end.append(idx)
-    run_end.append(len(arr.values))
-    return REIndex(name=arr.name, values=values, run_end=run_end)
+        else:
+            run_end[-1] = idx
+        last = val
+    return RunEndIndex(name=arr.name, values=values, run_end=run_end)
 
 
-def de_encode(arr: ArrayIndex) -> DEIndex:
+@overload
+def de_encode(arr: Array) -> DictEncodedArray: ...
+
+
+@overload
+def de_encode(arr: ArrayIndex) -> DictEncodedIndex: ...
+
+
+def de_encode(arr: ArrayIndex | Array) -> DictEncodedIndex | DictEncodedArray:
     # not using list(set(...)) to preserve order
     values = list(dict.fromkeys(arr.values))
     indices = list(map(values.index, arr.values))
-    return DEIndex(name=arr.name, values=values, indices=indices)
+    return DictEncodedIndex(name=arr.name, values=values, indices=indices)
 
 
-def series_to_col(col: pd.Series) -> ArrayIndex | DEIndex | Array | DEArray:
+def series_to_col(
+    col: pd.Series,
+) -> ArrayIndex | DictEncodedIndex | Array | DictEncodedArray:
     match col.name, col.dtype.type:
-        case "value", t if issubclass(t, bool | str) or t is object:
-            print(f"idx_type: {t}, value: {col.iloc[:3]}")
-            col = col.astype("category")
-            arr = DEArray(
-                name=col.name,
-                values=col.cat.categories,
-                indices=col.cat.codes,
-            )
-        case _, t if issubclass(t, int | float):
-            arr = Array(name=col.name, values=col.values)
-        case _, t if issubclass(t, str) or t is object:
+        case "value", t if issubclass(t, str) or t is object:
             print(f"type: {t}, value: {col.iloc[:3]}")
             col = col.astype("category")
-            arr = DEIndex(
+            return DictEncodedArray(
                 name=col.name,
                 values=col.cat.categories,
                 indices=col.cat.codes,
             )
-        case _, t if issubclass(t, int | str | datetime | timedelta) or t is object:
-            arr = ArrayIndex(name=col.name, values=col.values)
-        case _, _:
-            raise NotImplementedError(f"unknown type {t}")
-    return arr
+        case "value", t if issubclass(t, int):
+            return Array(name=col.name, values=col.values)
+        case _, t if issubclass(t, (bool, float, bytes)):
+            return Array(name=col.name, values=col.values)
+        case _, t if issubclass(t, str) or t is object:
+            print(f"idx_type: {t}, value: {col.iloc[:3]}")
+            col = col.astype("category")
+            return DictEncodedIndex(
+                name=col.name,
+                values=col.cat.categories,
+                indices=col.cat.codes,
+            )
+        case _, t if issubclass(t, (int, datetime, timedelta)) or t is object:
+            return ArrayIndex(name=col.name, values=col.values)
+        case n, t:
+            raise NotImplementedError(f"{n}: unknown type {t}")
 
 
 def to_tables(df: pd.DataFrame) -> Table:
     if df.empty:
         return []
-    return [series_to_col(df[colname]) for colname in df.columns]
+    return [series_to_col(col) for _, col in df.items()]
 
 
 if __name__ == "__main__":
@@ -117,20 +151,12 @@ if __name__ == "__main__":
 
     parser = ArgumentParser(__doc__)
     parser.add_argument("old_json")
-    #parser.add_argument("new_json")
+    parser.add_argument("new_json")
     opts = parser.parse_args()
-    new_json = opts.old_json.replace("/0_orig/", "/1_reencoded/")  # FIXME brittle
 
     df = to_df(json.loads(Path(opts.old_json).read_text()))
-    print("== DF ==")
-    #print(df)
     tbls = to_tables(df)
-    print("== TBLS ==")
-    print(tbls)
-    model = RootModel[Table](tbls).model_dump_json(indent=2)
-    print("== MODEL ==")
-    print(model)
     # NOTE: using pydantic is optional here, we can also write our own
     # JSON serialisation if we want, probably all we need is `asdict(...)`.
-    #Path(opts.new_json).write_text(RootModel[Table](tbls).model_dump_json())
-    Path(new_json).write_text(model)
+    json_blob = RootModel[Table](tbls).model_dump_json(indent=2)
+    Path(opts.new_json).write_text(json_blob)
