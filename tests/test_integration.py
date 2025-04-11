ade650964398fb416701456300865e8349bce797
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2025-04-11 17:52:27 +0200

    tests/test_integration: refactor for numpy array comparison

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,11 +1,20 @@
+from dataclasses import astuple
 import json
 
+import numpy as np
 import pytest
 
 from arrow_expts.schema.models import Array
 from arrow_expts.schema.reencode import series_to_col, to_df, to_tables
 
 from .conftest import JSONDIR
+
+
+def np_eq(i, j):
+    if isinstance(cmp := (i == j), np.ndarray):
+        return cmp.all()
+    else:
+        return cmp
 
 
 @pytest.mark.parametrize("fname,arr", [("array.numbers.json", [2.3, 23.0, 5.0])])
@@ -16,5 +25,10 @@ def test_json_to_tbl(fname, arr):
     exp = Array(name="i", values=arr)
     df = to_df(data)
 
-    assert series_to_col(df[exp.name]) == exp
-    assert to_tables(df) == [exp]
+    res = series_to_col(df[exp.name])
+    for i, j in zip(astuple(res), astuple(exp)):
+        assert np_eq(i, j)
+
+    tbl = to_tables(df)
+    assert len(tbl) == 1
+    assert isinstance(tbl[0], Array)
